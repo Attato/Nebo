58a3d7fb9a2ff52a461e825a92cdeffaf9d2ffa3
KoffeMeow <guyden999@mail.ru>
2022-10-12 00:37:52 +0300

    minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from 'styles/index.module.scss';
 
 import Header from 'components/header/header';
