14f16f3567123d46b4f9adf4a43d6afc1c31e861
KoffeMeow <guyden999@mail.ru>
2022-10-25 23:52:07 +0300

    prisma files upd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,17 @@ import 'styles/globals.scss';
 import 'styles/nullstyle.scss';
 
 import type { AppProps } from 'next/app';
+import { SessionProvider, SessionProviderProps } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+function MyApp({
+	Component,
+	pageProps: { session, ...pageProps },
+}: AppProps<SessionProviderProps>) {
+	return (
+		<SessionProvider session={session}>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
 }
 
 export default MyApp;
