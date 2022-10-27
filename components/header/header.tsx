9a908ecb5ba893e5c9d096c595f06aa92ca254bf
KoffeMeow <guyden999@mail.ru>
2022-10-27 21:24:48 +0300

    syntax change

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -8,29 +8,21 @@ const Header = () => {
 			<div className={styles.header__content}>
 				<div className={styles.right__side}>
 					<Link href="/">
-						<a>
-							<Image src="/svg/home.svg" width={24} height={24} alt="svg" />
-							<p>Главная</p>
-						</a>
+						<Image src="/svg/home.svg" width={24} height={24} alt="svg" />
+						<p>Главная</p>
 					</Link>
 					<Link href="/movies">
-						<a>
-							<Image src="/svg/movie.svg" width={24} height={24} alt="svg" />
-							<p>Фильмы</p>
-						</a>
+						<Image src="/svg/movie.svg" width={24} height={24} alt="svg" />
+						<p>Фильмы</p>
 					</Link>
 					<Link href="/cartoons">
-						<a>
-							<Image src="/svg/ticket.svg" width={24} height={24} alt="svg" />
-							<p>Билеты</p>
-						</a>
+						<Image src="/svg/ticket.svg" width={24} height={24} alt="svg" />
+						<p>Билеты</p>
 					</Link>
 				</div>
 
 				<div className={styles.left__side}>
-					<Link href="/signIn">
-						<a>Войти</a>
-					</Link>
+					<Link href="/signIn">Войти</Link>
 				</div>
 			</div>
 		</header>
