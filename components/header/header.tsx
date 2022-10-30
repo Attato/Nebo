de3e51011688f83efd07e67900996a5b3c82d9f9
KoffeMeow <guyden999@mail.ru>
2022-10-31 02:17:45 +0300

    hamburger menu

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,24 +1,51 @@
+import { useState } from 'react';
+
 import Link from 'next/link';
 import Image from 'next/image';
+
+import { motion } from 'framer-motion';
+
 import styles from './header.module.scss';
 
 const Header = () => {
+	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<header className={styles.header}>
 			<div className={styles.header__content}>
 				<div className={styles.right__side}>
-					<Link href="/">
-						<Image src="/svg/home.svg" width={24} height={24} alt="svg" />
-						<p>Главная</p>
-					</Link>
-					<Link href="/movies">
-						<Image src="/svg/movie.svg" width={24} height={24} alt="svg" />
-						<p>Фильмы</p>
-					</Link>
-					<Link href="/cartoons">
-						<Image src="/svg/ticket.svg" width={24} height={24} alt="svg" />
-						<p>Билеты</p>
-					</Link>
+					<button onClick={() => setIsOpen(!isOpen)} className={styles.burger}>
+						<span></span>
+					</button>
+					<div className={isOpen ? styles.menu__active : styles.menu}>
+						<div className={styles.menu__content}>
+							<Link href="/" onClick={() => setIsOpen(!isOpen)}>
+								<Image src="/svg/home.svg" width={24} height={24} alt="svg" />
+								<p>Главная</p>
+							</Link>
+							<Link href="/movies" onClick={() => setIsOpen(!isOpen)}>
+								<Image src="/svg/movie.svg" width={24} height={24} alt="svg" />
+								<p>Фильмы</p>
+							</Link>
+							<Link href="/cartoons" onClick={() => setIsOpen(!isOpen)}>
+								<Image src="/svg/ticket.svg" width={24} height={24} alt="svg" />
+								<p>Подписка</p>
+							</Link>
+						</div>
+					</div>
+					<div className={styles.right__side__content}>
+						<Link href="/">
+							<Image src="/svg/home.svg" width={24} height={24} alt="svg" />
+							<p>Главная</p>
+						</Link>
+						<Link href="/movies">
+							<Image src="/svg/movie.svg" width={24} height={24} alt="svg" />
+							<p>Фильмы</p>
+						</Link>
+						<Link href="/cartoons">
+							<Image src="/svg/ticket.svg" width={24} height={24} alt="svg" />
+							<p>Подписка</p>
+						</Link>
+					</div>
 				</div>
 
 				<div className={styles.left__side}>
