7cdc36459d95520a7721241cf292a4688934aebd
KoffeMeow <guyden999@mail.ru>
2022-10-31 02:18:07 +0300

    fix packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,9 @@ import 'styles/globals.scss';
 import 'styles/nullstyle.scss';
 
 import type { AppProps } from 'next/app';
-import { SessionProvider, SessionProviderProps } from 'next-auth/react';
 
-function MyApp({
-	Component,
-	pageProps: { session, ...pageProps },
-}: AppProps<SessionProviderProps>) {
-	return (
-		<SessionProvider session={session}>
-			<Component {...pageProps} />
-		</SessionProvider>
-	);
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+	return <Component {...pageProps} />;
 }
 
 export default MyApp;
