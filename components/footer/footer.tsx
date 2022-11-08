2500ab7c791832b74c4f213fa49e963b5b720877
KoffeMeow <guyden999@mail.ru>
2022-11-08 21:24:41 +0300

    relocate svg

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -13,18 +13,28 @@ const Footer = () => {
 					<p>следите за нами в соц. сетях.</p>
 					<div className={styles.social__icons}>
 						<Link href="https://vk.com/qwaqwakva" target="blank">
-							<Image src="/svg/vk.svg" width={20} height={20} alt="link" />
+							<Image
+								src="/svg/social/vk.svg"
+								width={20}
+								height={20}
+								alt="link"
+							/>
 						</Link>
 						<Link href="https://t.me/qwaqwakwa" target="blank">
 							<Image
-								src="/svg/telegram.svg"
+								src="/svg/social/telegram.svg"
 								width={20}
 								height={20}
 								alt="link"
 							/>
 						</Link>
 						<Link href="https://github.com/Attato" target="blank">
-							<Image src="/svg/github.svg" width={20} height={20} alt="link" />
+							<Image
+								src="/svg/social/github.svg"
+								width={20}
+								height={20}
+								alt="link"
+							/>
 						</Link>
 					</div>
 				</div>
@@ -33,15 +43,15 @@ const Footer = () => {
 						<h2>Разделы</h2>
 						<Link href="/">Главная</Link>
 						<Link href="/movies">Фильмы</Link>
-						<Link href="/tickets">Билеты</Link>
+						<Link href="/ticket">Подписка</Link>
 						<Link href="/signIn">Кабинет</Link>
 					</div>
 					<div className={styles.columns}>
 						<h2>Информация</h2>
-						<Link href="/movies">Документация</Link>
-						<Link href="/movies">Скидки</Link>
-						<Link href="/tickets">FAQ</Link>
-						<Link href="/movies">Политика конфиденциальности</Link>
+						<Link href="/*">Документация</Link>
+						<Link href="/*">Скидки</Link>
+						<Link href="/*">FAQ</Link>
+						<Link href="/*">Политика конфиденциальности</Link>
 					</div>
 				</div>
 			</div>
