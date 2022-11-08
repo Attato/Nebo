db6d2e4a0c2e499353d207dc91443713f1efb7e4
KoffeMeow <guyden999@mail.ru>
2022-11-08 21:25:41 +0300

    style update

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,33 +1,53 @@
 import { useState } from 'react';
 
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
-
-import { motion } from 'framer-motion';
 
 import styles from './header.module.scss';
 
 const Header = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const pathname = useRouter().pathname;
+
+	console.log(pathname);
+
 	return (
 		<header className={styles.header}>
 			<div className={styles.header__content}>
 				<div className={styles.right__side}>
-					<button onClick={() => setIsOpen(!isOpen)} className={isOpen ? styles.burger__active : styles.burger} />
+					<button
+						onClick={() => setIsOpen(!isOpen)}
+						className={isOpen ? styles.burger__active : styles.burger}
+					/>
 					<div className={isOpen ? styles.menu__active : styles.menu}>
-					{isOpen && (
-					<div className={styles.blur} onClick={() => setIsOpen(false)} />
-				)}
+						{isOpen && (
+							<div className={styles.blur} onClick={() => setIsOpen(false)} />
+						)}
 						<div className={styles.menu__content}>
 							<Link href="/" onClick={() => setIsOpen(!isOpen)}>
-								<Image src="/svg/home.svg" width={24} height={24} alt="svg" />
+								{pathname === '/' ? (
+									<Image
+										src="/svg/home/home_active.svg"
+										width={24}
+										height={24}
+										alt="svg"
+									/>
+								) : (
+									<Image
+										src="/svg/home/home.svg"
+										width={24}
+										height={24}
+										alt="svg"
+									/>
+								)}
 								<p>Главная</p>
 							</Link>
 							<Link href="/movies" onClick={() => setIsOpen(!isOpen)}>
 								<Image src="/svg/movie.svg" width={24} height={24} alt="svg" />
 								<p>Фильмы</p>
 							</Link>
-							<Link href="/cartoons" onClick={() => setIsOpen(!isOpen)}>
+							<Link href="/subscription" onClick={() => setIsOpen(!isOpen)}>
 								<Image src="/svg/ticket.svg" width={24} height={24} alt="svg" />
 								<p>Подписка</p>
 							</Link>
@@ -35,22 +55,66 @@ const Header = () => {
 					</div>
 					<div className={styles.right__side__content}>
 						<Link href="/">
-							<Image src="/svg/home.svg" width={24} height={24} alt="svg" />
+							{pathname === '/' ? (
+								<Image
+									src="/svg/home/home_active.svg"
+									width={24}
+									height={24}
+									alt="svg"
+								/>
+							) : (
+								<Image
+									src="/svg/home/home.svg"
+									width={24}
+									height={24}
+									alt="svg"
+									style={{ paddingBottom: 1 }}
+								/>
+							)}
 							<p>Главная</p>
 						</Link>
 						<Link href="/movies">
-							<Image src="/svg/movie.svg" width={24} height={24} alt="svg" />
+							{pathname === '/movies' ? (
+								<Image
+									src="/svg/movies/movies_active.svg"
+									width={24}
+									height={24}
+									alt="svg"
+									style={{ paddingBottom: 1 }}
+								/>
+							) : (
+								<Image
+									src="/svg/movies/movies.svg"
+									width={24}
+									height={24}
+									alt="svg"
+								/>
+							)}
 							<p>Фильмы</p>
 						</Link>
-						<Link href="/cartoons">
-							<Image src="/svg/ticket.svg" width={24} height={24} alt="svg" />
+						<Link href="/ticket">
+							{pathname === '/ticket' ? (
+								<Image
+									src="/svg/ticket/ticket_active.svg"
+									width={24}
+									height={24}
+									alt="svg"
+								/>
+							) : (
+								<Image
+									src="/svg/ticket/ticket.svg"
+									width={24}
+									height={24}
+									alt="svg"
+								/>
+							)}
 							<p>Подписка</p>
 						</Link>
 					</div>
 				</div>
 
 				<div className={styles.left__side}>
-					<Link href="/signIn">Войти</Link>
+					<Link href="/signIn">войдёшь?</Link>
 				</div>
 			</div>
 		</header>
