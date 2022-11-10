4408141bef378db316f74a815be47aed0b82122d
KoffeMeow <guyden999@mail.ru>
2022-11-10 21:28:50 +0300

    dropdown init

---
+++ components/dropdown/dropdown.tsx
@@ -0,0 +1,55 @@
+import { useState, useEffect, useRef } from 'react';
+import styles from './dropdown.module.scss';
+
+const Dropdown = ({ options }) => {
+	const [isActive, setIsActive] = useState(false);
+	const [selected, setSelected] = useState(options[0]);
+	const ref = useRef();
+
+	useEffect(() => {
+		const closeDropdown = (e) => {
+			if (e.path[0] !== ref.current) {
+				setIsActive(false);
+			}
+		};
+
+		document.body.addEventListener('click', closeDropdown);
+
+		return () => document.body.removeEventListener('click', closeDropdown);
+	}, []);
+
+	return (
+		<div className={styles.dropdown}>
+			<div
+				className={
+					isActive ? styles.dropdown__btn__active : styles.dropdown__btn
+				}
+				onClick={(e) => setIsActive(!isActive)}
+				ref={ref}
+			>
+				{selected}
+				<span className={styles.caret} />
+			</div>
+			{isActive && (
+				<div className={styles.dropdown__content}>
+					{options.map((option, id) => {
+						return (
+							<div
+								className={styles.dropdown__item}
+								key={id}
+								onClick={(e) => {
+									setSelected(option);
+									setIsActive(false);
+								}}
+							>
+								{option}
+							</div>
+						);
+					})}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default Dropdown;
