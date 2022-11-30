c66b119d6b46c3f559bfa5d2dc4892562341a1ad
KoffeMeow <guyden999@mail.ru>
2022-11-30 12:08:17 +0300

    hook init

---
+++ pages/movies/hooks/usePopularMovies.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export const usePopularMovies = () => {
+	const [popularMovies, setPopularMovies] = useState([]);
+
+	const fetchPopularMovies = async () => {
+		const data = await fetch(
+			'https://api.themoviedb.org/3/movie/popular?api_key=4506fc01a7d9f8d5b35ff79dc50bf10c&language=en-US&page=1'
+		);
+
+		const movies = await data.json();
+		setPopularMovies(movies.results);
+	};
+
+	useEffect(() => {
+		fetchPopularMovies();
+	}, []);
+
+	return { popularMovies };
+};
