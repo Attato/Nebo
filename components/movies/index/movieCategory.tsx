826f1faf318ea80ae7af888adff2099a444a4c80
KoffeMeow <guyden999@mail.ru>
2022-12-11 18:30:31 +0300

    removed the condition

--- components/movies/index/movieCategory.tsx
+++ components/movies/index/movieCategory.tsx
@@ -4,29 +4,27 @@ import Link from 'next/link';
 
 const MovieCategory = ({ movie }) => {
 	return (
-		movie.original_language === 'en' && (
-			<motion.div
-				className={styles.card}
-				layout
-				animate={{ opacity: 1 }}
-				initial={{ opacity: 0 }}
-				exit={{ opacity: 0 }}
-			>
-				<Link href={`/movies/${movie.original_title.replace(/\s/g, '')}`}>
-					<img
-						src={movie.backdrop_path}
-						width={220}
-						height={180}
-						alt="image"
-						draggable="false"
-					/>
-					<h3>
-						{movie.vote_average.toFixed(1)} &nbsp; {movie.title}
-					</h3>
-					<p>{movie.overview}</p>
-				</Link>
-			</motion.div>
-		)
+		<motion.div
+			className={styles.card}
+			layout
+			animate={{ opacity: 1 }}
+			initial={{ opacity: 0 }}
+			exit={{ opacity: 0 }}
+		>
+			<Link href={`/movies/${movie.slug}`}>
+				<img
+					src={movie.backdrop_path}
+					width={220}
+					height={180}
+					alt="image"
+					draggable="false"
+				/>
+				<h3>
+					{movie.vote_average.toFixed(1)} &nbsp; {movie.title}
+				</h3>
+				<p>{movie.overview}</p>
+			</Link>
+		</motion.div>
 	);
 };
 
