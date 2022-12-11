9c227b3daa98a608c0443fefee9387f5ef388f3a
KoffeMeow <guyden999@mail.ru>
2022-12-12 00:46:54 +0300

    header update

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,16 +4,21 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
 
+import pages from './header.json';
+
 import styles from './header.module.scss';
 
 const Header = () => {
 	const [isOpen, setIsOpen] = useState(false);
-	const pathname = useRouter().pathname;
+	const router = useRouter();
+
+	console.log(router.route);
 
 	return (
 		<header className={styles.header}>
 			<div className={styles.header__content}>
 				<div className={styles.right__side}>
+					{/* Mobile */}
 					<button
 						onClick={() => setIsOpen(!isOpen)}
 						className={isOpen ? styles.burger__active : styles.burger}
@@ -28,13 +33,23 @@ const Header = () => {
 							<Link href="/subscribe?product=3">Подписка</Link>
 						</div>
 					</div>
+					{/* Desktop */}
 					<div className={styles.right__side__content}>
-						<Link href="/">Главная</Link>
-						<Link href="/movies">Фильмы</Link>
-						<Link href="/subscribe?product=3">Подписка</Link>
-						<Link href={{ pathname: '/user', query: { tab: 'Профиль' } }}>
-							test user
-						</Link>
+						{pages.map((page, id) => {
+							return (
+								<Link
+									href={page.link + `${page.query}`}
+									key={id}
+									className={
+										router.route === page.link
+											? styles.link__active
+											: styles.link
+									}
+								>
+									{page.page}
+								</Link>
+							);
+						})}
 					</div>
 				</div>
 
